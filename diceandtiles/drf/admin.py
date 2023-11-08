c47a560c426e4c4d018d4a886872fa024c4cf771
Hakkene <dogier2006@gmail.com>
2023-11-08 07:13:41 +0100

    created models, registered them in admin panel, created drf app

---
+++ diceandtiles/drf/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Product, Comment, Profile, Vote
+
+
+
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','id','slug']
+
+@admin.register(Comment)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['product','body','created_on','owner']
+
+@admin.register(Profile)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','user']
+    
+@admin.register(Vote)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['product','value','owner']
