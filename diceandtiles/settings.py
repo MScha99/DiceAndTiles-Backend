3d57b72575b26dd49691000a08d19ae853e1de0d
Hakkene <dogier2006@gmail.com>
2024-01-21 23:33:56 +0100

    gzip middleware django

--- diceandtiles/settings.py
+++ diceandtiles/settings.py
@@ -54,6 +54,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware', #gzip 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
